808ba0b75d5deca8e42c914ca39ce0a5b0f20838
Roy-Ermers <Roy-Ermers@users.noreply.github.com>
2021-11-21 23:14:01 +0000

    deploy: 4dee132e5dcd81088bf7cbff8824c767d4fe1c44

---
+++ Runtime/index.js
@@ -0,0 +1,21 @@
+import {
+  Color,
+  Component,
+  Entity,
+  Keyboard,
+  Mouse,
+  Runtime_default,
+  TileMap,
+  Vector2
+} from "../chunk-UVEQVABF.js";
+export {
+  Color,
+  Component,
+  Entity,
+  Keyboard,
+  Mouse,
+  TileMap as Tilemap,
+  Vector2,
+  Runtime_default as default
+};
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFtdLAogICJzb3VyY2VzQ29udGVudCI6IFtdLAogICJtYXBwaW5ncyI6ICIiLAogICJuYW1lcyI6IFtdCn0K
